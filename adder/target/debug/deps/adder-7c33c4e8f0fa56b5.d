dc6498eac31792e46f6e3016eee587f0717402aa
ahmed-elsakka <ahmed.elsakka@outlook.com>
2025-05-28 16:51:18 +0200

    Add adder project

---
+++ adder/target/debug/deps/adder-7c33c4e8f0fa56b5.d
@@ -0,0 +1,5 @@
+C:\Projects\Courses\Rust Course\rust_course\adder\target\debug\deps\libadder-7c33c4e8f0fa56b5.rmeta: src\lib.rs
+
+C:\Projects\Courses\Rust Course\rust_course\adder\target\debug\deps\adder-7c33c4e8f0fa56b5.d: src\lib.rs
+
+src\lib.rs:
